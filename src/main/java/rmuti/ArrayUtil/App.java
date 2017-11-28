692e9e5aab4fe97c3d71edaeae58acdf911c7e32
chophaka <cho25531@gmail.com>
2017-11-28 17:45:53 +0700

    love

---
+++ src/main/java/rmuti/ArrayUtil/App.java
@@ -0,0 +1,13 @@
+package rmuti.ArrayUtil;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
